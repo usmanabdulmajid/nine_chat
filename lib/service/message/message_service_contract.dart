0d2ac65fc1b433ac97475a967877c9ad50d9e6a4
Abdulmajid Usman <abstractmajid@gmail.com>
2021-11-27 14:31:36 +0100

    add typing service

--- lib/service/message/message_service_contract.dart
+++ lib/service/message/message_service_contract.dart
@@ -4,5 +4,6 @@ abstract class IMessageService {
   Future<bool> send(Message message);
   Stream<Message> messages(String userId);
   Future<List<Message>> retrieveMessages(String userId);
+  Future<bool> replyMessageIndex(Message message);
   void dispose();
 }
